4ea3097083a8951feaecc4220ec243d791d295da
Jared Seehusahai <pagaley12@outlook.com>
2018-02-11 00:15:51 -0500

    Added a scaffold, posts wiht description, name and price

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180211033732) do
+ActiveRecord::Schema.define(version: 20180211035648) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "description"
+    t.string "name"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
