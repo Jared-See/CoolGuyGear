4ae8d694fe2f2f2b2aa17ed7b7d3f8d95322aee4
Jared Seehusahai <pagaley12@outlook.com>
2018-02-22 17:55:28 -0500

    Added user id to psots, can no longer access the posts page unless youa re logged in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180221005733) do
+ActiveRecord::Schema.define(version: 20180222224803) do
 
   create_table "posts", force: :cascade do |t|
     t.string "description"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20180221005733) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "questions", force: :cascade do |t|
