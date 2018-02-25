71d98e73dd973fc53d4eb149c254ffba31a10d0b
Jared Seehusahai <pagaley12@outlook.com>
2018-02-25 15:51:36 -0500

    Added Paperclip Gem with Image Magick and added an image collum to the posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180222224803) do
+ActiveRecord::Schema.define(version: 20180225204632) do
 
   create_table "posts", force: :cascade do |t|
     t.string "description"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20180222224803) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "questions", force: :cascade do |t|
